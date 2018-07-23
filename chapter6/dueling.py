d6b9a9652f2edc0dff60882fa8d2703af7ab6cf1
aniharu <haruya04183083@gmail.com>
2018-07-23 14:16:06 +0900

    👍

--- chapter6/dueling.py
+++ chapter6/dueling.py
@@ -1,3 +1,5 @@
+# double DQN の実装
+
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
@@ -58,48 +60,82 @@ class Network(nn.Module):
         h2 = F.relu(self.fc2(h1))
         out = self.fc3(h2)
         return out
+class Net(nn.Module):
+    def __init__(self, n_in, n_mid, n_out):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(n_in, n_mid)
+        self.fc2 = nn.Linear(n_mid, n_mid)
+        self.fc3_adv = nn.Linear(n_mid, n_out)
+        self.fc3_v = nn.Linear(n_mid, 1)
+    def forward(self, x):
+        h1 = F.relu(self.fc1(x))
+        h2 = F.relu(self.fc2(h1))
+        adv = self.fc3_adv(h2)
+        val = self.fc3_v(h2).expand(-1, adv.size(1))
+        output = val + adv - adv.mean(1, keepdim=True).expand(-1, adv.size(1))
+        return output
 
 class Brain:
     def __init__(self, num_states, num_actions):
         self.num_actions = num_actions
         self.memory = ReplayMemory(CAPACITY)
-        self.model = Network(num_states, num_actions)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
+        n_in, n_mid, n_out = num_states, 32, num_actions
+        self.main_q_network = Net(n_in, n_mid, n_out)
+        self.target_q_network = Net(n_in, n_mid, n_out)
+        self.optimizer = optim.Adam(self.main_q_network.parameters(), lr=0.0001)
 
     def replay(self):
-        if len(self.memory)<BATCH_SIZE:
+        if len(self.memory) < BATCH_SIZE:
             return
+
+        self.batch, self.state_batch, self.action_batch, self.reward_batch, self.non_final_next_states = self.make_minibatch()
+        self.expected_state_action_values = self.get_expected_state_action_values()
+        self.update_main_q_network()
+
+    def decide_action(self, state, episode):
+        epsilon = 0.5 * (1 / (episode + 1))
+        if epsilon <= np.random.uniform(0, 1):
+            self.main_q_network.eval()
+            with torch.no_grad():
+                action = self.main_q_network(state).max(1)[1].view(1, 1)
+        else:
+            action = torch.LongTensor(
+                [[random.randrange(self.num_actions)]]
+            )
+        return action
+
+    def make_minibatch(self):
         transitions = self.memory.sample(BATCH_SIZE)
         batch = Transition(*zip(*transitions))
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
         non_final_next_states = torch.cat([s for s in batch.state_next if s is not None])
+        return batch, state_batch, action_batch, reward_batch, non_final_next_states
 
-        self.model.eval()
-        state_action_values = self.model(state_batch).gather(1, action_batch)
-        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None, batch.state_next)))
+    def get_expected_state_action_values(self):
+        self.main_q_network.eval()
+        self.target_q_network.eval()
+        self.state_action_values = self.main_q_network(self.state_batch).gather(1, self.action_batch)
+        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None, self.batch.state_next)))
         next_state_values = torch.zeros(BATCH_SIZE)
+        a_m = torch.zeros(BATCH_SIZE).type(torch.LongTensor)
+        a_m[non_final_mask]=self.main_q_network(self.non_final_next_states).detach().max(1)[1]
+        a_m_non_final_next_states = a_m[non_final_mask].view(-1, 1)
 
-        next_state_values[non_final_mask] = self.model(non_final_next_states).max(1)[0].detach()
-        expected_state_action_value = reward_batch + GAMMA * next_state_values
-        self.model.train()
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_value.unsqueeze(1))
+        next_state_values[non_final_mask]=self.target_q_network(self.non_final_next_states).gather(1, a_m_non_final_next_states).detach().squeeze()
+        expected_state_action_values = self.reward_batch + GAMMA * next_state_values
+        return expected_state_action_values
+
+    def update_main_q_network(self):
+        self.main_q_network.train()
+        loss = F.smooth_l1_loss(self.state_action_values, self.expected_state_action_values.unsqueeze(1))
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-    def decide_action(self, state, episode):
-        epsilon = 0.5 * (1 / (episode + 1))
-        if epsilon <= np.random.uniform(0, 1):
-            self.model.eval()
-            with torch.no_grad():
-                action = self.model(state).max(1)[1].view(1, 1)
-        else:
-            action = torch.LongTensor(
-                [[random.randrange(self.num_actions)]]
-            )
-        return action
+    def update_target_q_network(self):
+        self.target_q_network.load_state_dict(self.main_q_network.state_dict())
 
 class Agent:
     def __init__(self, num_states, num_actions):
@@ -111,6 +147,8 @@ class Agent:
         return action
     def memorize(self, state, action, state_next, reward):
         self.brain.memory.push(state, action, state_next, reward)
+    def update_target_q_function(self):
+        self.brain.update_target_q_network()
 
 class Environment:
     def __init__(self):
@@ -157,6 +195,8 @@ class Environment:
 
                 if done:
                     print('%d Episode: Finished after %d steps: 10Average = %0.1f' % (episode, step + 1, episode_10_list.mean()))
+                    if((episode % 2) == 0):
+                        self.agent.update_target_q_function()
                     break
             if episode_final is True or episode == NUM_EPISODES-1:
                 #frame_save(frames)
@@ -172,4 +212,4 @@ if __name__=='__main__':
     for i in range(10):
         env = Environment()
         result += env.run()
-    print(result/10)
+    print(result / 10)
---
+++ chapter6/priorized.py
@@ -0,0 +1,285 @@
+# double DQN の実装
+
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import random
+from torch import optim
+from matplotlib import animation
+from collections import namedtuple
+
+######################################
+# 定数
+ENV = 'CartPole-v0'
+GAMMA = 0.99
+MAX_STEPS = 200
+NUM_EPISODES = 500
+BATCH_SIZE = 32
+CAPACITY = 10000
+TD_ERROR_EPSILON = 0.0001
+######################################
+
+Transition = namedtuple('Transition',('state','action','state_next','reward'))
+
+def frame_save(frames):
+    plt.figure(figsize=(frames[0].shape[1]/72.0, frames[0].shape[0]/72.0), dpi=72)
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(),animate, frames=len(frames), interval=50)
+    anim.save('out.html')
+class ReplayMemory:
+    def __init__(self, CAPACITY):
+        self.capacity = CAPACITY
+        self.memory = []
+        self.index = 0
+    def push(self, state, action, state_next, reward):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.index] = Transition(state, action, state_next, reward)
+        self.index = (self.index + 1)%self.capacity
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    def __len__(self):
+        return len(self.memory)
+class TDerrorMemory:
+    def __init__(self, CAPACITY):
+        self.capacity = CAPACITY
+        self.memory = []
+        self.index = 0
+    def push(self, td_error):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.index] = td_error
+        self.index = (self.index + 1)%self.capacity
+    def __len__(self):
+        return len(self.memory)
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    def __len__(self):
+        return len(self.memory)
+    def get_prioritized_indexes(self, batch_size):
+        sum_absolute_td_error = np.sum(np.absolute(self.memory))
+        sum_absolute_td_error += TD_ERROR_EPSILON * len(self.memory)
+
+        rand_list = np.random.uniform(0, sum_absolute_td_error, batch_size)
+        rand_list = np.sort(rand_list)
+
+        indexes = []
+        idx = 0
+        tmp_sum_absolute_td_error = 0
+        for rand_num in rand_list:
+            while tmp_sum_absolute_td_error < rand_num:
+                tmp_sum_absolute_td_error += (
+                    abs(self.memory[idx]) + TD_ERROR_EPSILON)
+                idx += 1
+            if idx >= len(self.memory):
+                idx = len(self.memory)-1
+            indexes.append(idx)
+        return indexes
+    def update_td_error(self, updated_td_errors):
+        self.memory = updated_td_errors
+
+class Network(nn.Module):
+    def __init__(self, num_states, num_actions):
+        super(Network, self).__init__()
+        self.fc1 = nn.Linear(num_states, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, num_actions)
+    def forward(self, x):
+        h1 = F.relu(self.fc1(x))
+        h2 = F.relu(self.fc2(h1))
+        out = self.fc3(h2)
+        return out
+class Net(nn.Module):
+    def __init__(self, n_in, n_mid, n_out):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(n_in, n_mid)
+        self.fc2 = nn.Linear(n_mid, n_mid)
+        self.fc3_adv = nn.Linear(n_mid, n_out)
+        self.fc3_v = nn.Linear(n_mid, 1)
+    def forward(self, x):
+        h1 = F.relu(self.fc1(x))
+        h2 = F.relu(self.fc2(h1))
+        adv = self.fc3_adv(h2)
+        val = self.fc3_v(h2).expand(-1, adv.size(1))
+        output = val + adv - adv.mean(1, keepdim=True).expand(-1, adv.size(1))
+        return output
+
+class Brain:
+    def __init__(self, num_states, num_actions):
+        self.num_actions = num_actions
+        self.memory = ReplayMemory(CAPACITY)
+        n_in, n_mid, n_out = num_states, 32, num_actions
+        self.main_q_network = Net(n_in, n_mid, n_out)
+        self.target_q_network = Net(n_in, n_mid, n_out)
+        self.optimizer = optim.Adam(self.main_q_network.parameters(), lr=0.0001)
+        self.td_error_memory = TDerrorMemory(CAPACITY)
+
+    def replay(self, episode):
+        if len(self.memory) < BATCH_SIZE:
+            return
+
+        self.batch, self.state_batch, self.action_batch, self.reward_batch, self.non_final_next_states = self.make_minibatch(episode)
+        self.expected_state_action_values = self.get_expected_state_action_values()
+        self.update_main_q_network()
+
+    def decide_action(self, state, episode):
+        epsilon = 0.5 * (1 / (episode + 1))
+        if epsilon <= np.random.uniform(0, 1):
+            self.main_q_network.eval()
+            with torch.no_grad():
+                action = self.main_q_network(state).max(1)[1].view(1, 1)
+        else:
+            action = torch.LongTensor(
+                [[random.randrange(self.num_actions)]]
+            )
+        return action
+
+    def make_minibatch(self, episode):
+        if episode > 30:
+            transitions = self.memory.sample(BATCH_SIZE)
+        else:
+            indexes = self.td_error_memory.get_prioritized_indexes(BATCH_SIZE)
+            transitions = [self.memory.memory[n] for n in indexes]
+        batch = Transition(*zip(*transitions))
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+        non_final_next_states = torch.cat([s for s in batch.state_next if s is not None])
+        return batch, state_batch, action_batch, reward_batch, non_final_next_states
+
+    def get_expected_state_action_values(self):
+        self.main_q_network.eval()
+        self.target_q_network.eval()
+        self.state_action_values = self.main_q_network(self.state_batch).gather(1, self.action_batch)
+        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None, self.batch.state_next)))
+        next_state_values = torch.zeros(BATCH_SIZE)
+        a_m = torch.zeros(BATCH_SIZE).type(torch.LongTensor)
+        a_m[non_final_mask]=self.main_q_network(self.non_final_next_states).detach().max(1)[1]
+        a_m_non_final_next_states = a_m[non_final_mask].view(-1, 1)
+
+        next_state_values[non_final_mask]=self.target_q_network(self.non_final_next_states).gather(1, a_m_non_final_next_states).detach().squeeze()
+        expected_state_action_values = self.reward_batch + GAMMA * next_state_values
+        return expected_state_action_values
+
+    def update_main_q_network(self):
+        self.main_q_network.train()
+        loss = F.smooth_l1_loss(self.state_action_values, self.expected_state_action_values.unsqueeze(1))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def update_target_q_network(self):
+        self.target_q_network.load_state_dict(self.main_q_network.state_dict())
+
+    def update_td_error_memory(self):
+        self.main_q_network.eval()
+        self.target_q_network.eval()
+        transitions = self.memory.memory
+        batch = Transition(*zip(*transitions))
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+        non_final_next_states = torch.cat([s for s in batch.state_next if s is not None])
+
+        state_action_values = self.main_q_network(state_batch).gather(1, action_batch)
+        non_final_mask = torch.ByteTensor(tuple(map(lambda s: s is not None, batch.state_next)))
+        next_state_values =torch.zeros(len(self.memory))
+        a_m = torch.zeros(len(self.memory)).type(torch.LongTensor)
+
+        a_m[non_final_mask] = self.main_q_network(non_final_next_states).detach().max(1)[1]
+        a_m_non_final_next_states = a_m[non_final_mask].view(-1, 1)
+        next_state_values[non_final_mask]=self.target_q_network(non_final_next_states).gather(1, a_m_non_final_next_states).detach().squeeze()
+
+        td_errors = (reward_batch + GAMMA * next_state_values) - state_action_values.squeeze()
+        self.td_error_memory.memory = td_errors.detach().numpy().tolist()
+
+class Agent:
+    def __init__(self, num_states, num_actions):
+        self.brain = Brain(num_states, num_actions)
+    def update_q_function(self, episode):
+        self.brain.replay(episode)
+    def get_action(self, state, episode):
+        action = self.brain.decide_action(state, episode)
+        return action
+    def memorize(self, state, action, state_next, reward):
+        self.brain.memory.push(state, action, state_next, reward)
+    def update_target_q_function(self):
+        self.brain.update_target_q_network()
+    def memorize_td_error(self, td_error):
+        self.brain.td_error_memory.push(td_error)
+    def update_td_error_memory(self):
+        self.brain.update_td_error_memory()
+
+class Environment:
+    def __init__(self):
+        self.env = gym.make(ENV)
+        self.num_states = self.env.observation_space.shape[0]
+        self.num_actions = self.env.action_space.n
+        self.agent = Agent(self.num_states, self.num_actions)
+    def run(self):
+        episode_10_list = np.zeros(10)
+        complete_episodes = 0
+        episode_final = False
+        frames = []
+        for episode in range(NUM_EPISODES):
+            observation = self.env.reset()
+            state = observation
+            state = torch.from_numpy(state).type(torch.FloatTensor)
+            state = torch.unsqueeze(state, 0)
+
+            for step in range(MAX_STEPS):
+                if episode_final is True or episode == NUM_EPISODES-1:
+                    #frames.append(self.env.render(mode='rgb_array'))
+                    pass
+                action = self.agent.get_action(state, episode)
+                observation_next,_,done,_ = self.env.step(action.item())
+                if done:
+                    state_next = None
+                    episode_10_list = np.hstack((episode_10_list[1:], step + 1))
+                    if step < 195:
+                        reward = torch.FloatTensor([-1.0])
+                        complete_episodes = 0
+                    else:
+                        reward = torch.FloatTensor([1.0])
+                        complete_episodes = complete_episodes + 1
+
+                else:
+                    reward = torch.FloatTensor([0.0])
+                    state_next = observation_next
+                    state_next = torch.from_numpy(state_next).type(torch.FloatTensor)
+                    state_next = torch.unsqueeze(state_next, 0)
+
+                self.agent.memorize(state, action, state_next, reward)
+                self.agent.memorize_td_error(0)
+                self.agent.update_q_function(episode)
+                state = state_next
+
+                if done:
+                    print('%d Episode: Finished after %d steps: 10Average = %0.1f' % (episode, step + 1, episode_10_list.mean()))
+                    self.agent.update_td_error_memory()
+                    if((episode % 2) == 0):
+                        self.agent.update_target_q_function()
+                    break
+            if episode_final is True or episode == NUM_EPISODES-1:
+                #frame_save(frames)
+                break
+            if complete_episodes >= 10:
+                print('Success 10 episode!')
+                episode_final = True
+        return episode
+
+
+if __name__=='__main__':
+    result = 0
+    for i in range(10):
+        env = Environment()
+        result += env.run()
+    print(result / 10)
